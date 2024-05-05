b793198348a84012ccb5ede7834f9227a606ee72
FoxRed-cmd <blinovk955@gmail.com>
2024-05-05 22:57:49 +0300

    added NotFoundPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import cls from "./NotFoundPage.module.scss";
+
+export const NotFoundPage = () => {
+	return (
+		<div className={cls.NotFoundPage}>
+			<h1>Страница не найдена...</h1>
+		</div>
+	);
+};
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from "react-router-dom";
 import { MainPage } from "src/pages/MainPage";
 import { MovieDetailsPage } from "src/pages/MovieDetailsPage";
+import { NotFoundPage } from "src/pages/NotFoundPage";
 
 export enum AppRoutes {
   MAIN = "main",
   MOVIE_DETAILS = "movieDetails",
+  NOT_FOUND = "notFound",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
   [AppRoutes.MOVIE_DETAILS]: "/movie/:id",
+  [AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MOVIE_DETAILS]: {
     path: RoutePath.movieDetails,
     element: <MovieDetailsPage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.notFound,
+    element: <NotFoundPage />,
   },
 };
